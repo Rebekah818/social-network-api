983651bdd934a00eb30f0433f43ea55ff9299a60
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-04-11 23:03:05 -0400

    added the user and thought model to index.js model

--- models/index.js
+++ models/index.js
@@ -1,19 +1,5 @@
-const { Schema, model } = require('mongoose');
+const User = require('./User');
+const Thought = require('./Thought');
 
-const Schema = new Schema({
-    pizzaName: {
-      type: String
-    },
-    createdBy: {
-      type: String
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now
-    },
-    size: {
-      type: String,
-      default: 'Large'
-    },
-    toppings: []
-  });
+
+module.exports = {User, Thought};
