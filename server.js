90c202dfef77913bc0f706a25d48170b9e6ce1d9
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-04-13 11:01:22 -0400

    changes to package.json and server.js

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.use(require('./routes/index'));
 
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
-    useFindAndModify: false,
+    // useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-mongoose.set('debug', true);
+// mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
 
